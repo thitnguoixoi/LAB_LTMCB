55b3ee0baa6c0e3969c9bd3998b0986dfafedb3e
c1nd3r3114 <88484431+hunger1607@users.noreply.github.com>
2023-04-11 01:11:09 +0700

    Fix main program bug

--- LTMCB_Lab02/LTMCB_Lab02/Lab02.Designer.cs
+++ LTMCB_Lab02/LTMCB_Lab02/Lab02.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(207, 45);
+            this.button1.Location = new System.Drawing.Point(310, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Bài 1";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +48,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(207, 125);
+            this.button2.Location = new System.Drawing.Point(310, 192);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 1;
             this.button2.Text = "Bài 2";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(207, 205);
+            this.button3.Location = new System.Drawing.Point(310, 315);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 2;
             this.button3.Text = "Bài 3";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,34 +70,37 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(207, 365);
+            this.button4.Location = new System.Drawing.Point(310, 436);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(112, 35);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Bài 5";
+            this.button4.Text = "Bài 4";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(207, 285);
+            this.button5.Location = new System.Drawing.Point(310, 557);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(112, 35);
             this.button5.TabIndex = 4;
-            this.button5.Text = "Bài 4";
+            this.button5.Text = "Bài 5";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Lab02
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 482);
+            this.ClientSize = new System.Drawing.Size(732, 742);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Lab02";
             this.Text = "Lab02";
             this.Load += new System.EventHandler(this.Lab02_Load);
